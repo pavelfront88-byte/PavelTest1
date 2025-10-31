f0e1396f81887c5ad0679b1408d2d6b896feeca9
Pavel <pavelfront88@>
2025-10-31 22:44:39 +0500

    add new branch with dz1 save

--- src/main/java/org/example/dz1.java
+++ src/main/java/org/example/dz1.java
@@ -1,4 +1,15 @@
 package org.example;
 
 public class dz1 {
+    public static void printThreeWords() {
+    System.out.println("Orange");
+    System.out.println("Banana");
+    System.out.println("Apple");
 }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+}
+
+
