d726d0efd08e046b9871c5170450809a6bd2fc10
Pavel <pavelfront88@>
2025-11-03 15:08:29 +0500

    add new branch with dz9 save

---
+++ src/main/java/org/example/dz4.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class dz4 {
+    public void compareNumbers() {
+        int a =10;
+        int b =5;
+        if (a >=b)
+        {
+            System.out.println("a>=b");
+        } else
+        {
+            System.out.println("a<b");
+        }
+    }
+    public static void main(String[] args)
+    {
+        dz4 instance = new dz4();
+        instance.compareNumbers();
+    }
+}
