c96062b49a38e6516765694adfed066bdc8f9367
Pavel <pavelfront88@>
2025-11-03 15:09:26 +0500

    add new branch with dz9 save

---
+++ src/main/java/org/example/dz5.java
@@ -0,0 +1,11 @@
+
+
+public class dz5 {
+    public static boolean isSumInRange(int a, int b)
+    {
+        int sum = a+b;
+        return sum >=10 && sum <=20;
+    }
+}
+public static void main(String[] args) {
+    System.out.println(dz5.isSumInRange (5, 15));}
