3867a6449eac90b1f397893bda7df0ebc45c35a2
Pavel <pavelfront88@>
2025-11-03 15:15:59 +0500

    add new branch with dz9 save

---
+++ src/main/java/org/example/dz7.java
@@ -0,0 +1,10 @@
+
+public class dz7 {
+    public static boolean isNegative(int number)
+    {
+        return number < 0;
+    }
+}
+public static void main(String[] args)
+{
+    System.out.println(dz7.isNegative(10));}
