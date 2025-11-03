3dc4f773eb32dc34de5e0870fd4c455f6582c604
Pavel <pavelfront88@>
2025-11-03 15:16:54 +0500

    add new branch with dz9 save

---
+++ src/main/java/org/example/dz8.java
@@ -0,0 +1,10 @@
+
+
+public class dz8 {
+    public static void printStringMultipleTimes(String text, int count) {
+    }
+}
+public static void main(String[] args)
+{
+    dz8.printStringMultipleTimes("Hi",1);
+}
