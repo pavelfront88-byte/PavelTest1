4119dfdf6739c852a442ae6a1634b71e3175c3a8
Pavel <pavelfront88@>
2025-11-03 15:15:07 +0500

    add new branch with dz9 save

---
+++ src/main/java/org/example/dz6.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class dz6 {public static void checkPositiveOrNegative(int number)
+{
+    if (number >= 0)
+    {
+        System.out.println("positive");
+    } else
+    {
+        System.out.println("negative");
+    }
+}
+    public static void main(String[] args)
+    {
+        dz6.checkPositiveOrNegative(5);
+    }
+}
