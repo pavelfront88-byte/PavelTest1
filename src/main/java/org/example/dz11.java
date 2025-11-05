4e8fb0766da5d6f3e8a33d42d3b9315a2a41b391
Pavel <pavelfront88@>
2025-11-05 21:11:56 +0500

    add new branch with dz1 save

---
+++ src/main/java/org/example/dz11.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class dz11 {
+    public static void main(String[] args) {
+        int[] array =new int[100];
+        for (int i =0; i <array.length;i++)
+        {array[i] =i +1;
+        }
+        for (int i =0; i <array.length;i++)
+        {
+            System.out.print(array[i] +"");
+        }}}
