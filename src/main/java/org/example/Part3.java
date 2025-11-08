f9e704db8769dc65e96aa0613b7ae0e3158e9523
Pavel <pavelfront88@>
2025-11-08 19:15:59 +0500

    add new branch with dz3 save

--- src/main/java/org/example/Part3.java
+++ src/main/java/org/example/Part3.java
@@ -1,4 +1,17 @@
 package org.example;
-
-public class Part3 {
-}
+public class Park {
+    private String name;
+    public class Attraction
+    {
+        private String name;
+        private String working_Hours;
+        private double cost;
+        public Attraction(
+                String name,
+                String working_Hours,
+                double cost)
+        {
+            this.name=name;
+            this.working_Hours=working_Hours;
+            this.cost=cost;
+        }}
