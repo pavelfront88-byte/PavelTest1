2313cea1674bb017cb686a07f82c28bffa04db35
Pavel <pavelfront88@>
2025-11-08 22:47:39 +0500

    add new branch save

--- src/main/java/org/example/Part1.java
+++ src/main/java/org/example/Part1.java
@@ -1,3 +1,49 @@
 package org.example;
-public class Part1 {
+class Animal
+{
+    protected String name;
+    public Animal(String name)
+    {
+        this.name=name;
+    }
+    void run(int distance)
+    {
+        System.out.println(name+"пробежал"+distance+"метров");
+    }
+    void swim(int distance)
+    {
+        System.out.println(name+"проплыл"+distance+"метров");
+    }
 }
+class Dog extends Animal
+{
+    public Dog(String Sharik)
+    {
+        super(Sharik);
+    }
+    void run(int distance)
+    {
+        if (distance<=500)
+            System.out.println(getName()+"пробежал"+distance+"метров");
+        else
+            System.out.println(getName()+"не смог пробежать дистанцию");
+    }
+    private String getName()
+    { return name; }
+}
+class Cat extends Animal
+{
+    public Cat(String Murzik)
+    {
+        super(Murzik);
+    }
+    void run(int distance)
+    {
+        if (distance<=200)
+            System.out.println(getName()+"пробежал"+distance+"метров");
+        else
+            System.out.println(getName()+"не смог пробежать дистанцию");
+    }
+    private String getName() { return name; }
+}
+
