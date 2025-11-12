511f1575ecd684012da2a11e133d3ae33a19bc36
Pavel <pavelfront88@>
2025-11-12 22:46:27 +0500

    add new branch save

--- src/main/java/org/example/Cat.java
+++ src/main/java/org/example/Cat.java
@@ -1,22 +1,27 @@
 package org.example;
 class Cat extends Animal
 {
-    private static int catCount = 0; // счётчик котов
-    private boolean isFull; // сытость кота
-    public Cat(String name) {
+    private static int catCount = 0;
+    private boolean isFull;
+    public Cat(String name)
+    {
         super(name);
         this.isFull = false;
         catCount++;
     }
     @Override
-    public void run(int distance) {
-        if (distance <= 200) {
-            System.out.println(name + " пробежал " + distance + " м.");
-        } else {
-            System.out.println(name + " не может пробежать " + distance + " м. (максимум 200 м)");
+    public void run(int distance)
+    {
+        if (distance <= 200)
+        {
+            System.out.println(name+distance);
+        } else
+        {
+            System.out.println(name +distance);
         }
     }
     @Override
-    public void swim(int distance) {
-        System.out.println(name + " не умеет плавать!");
+    public void swim(int distance)
+    {
+        System.out.println(name);
     }}
