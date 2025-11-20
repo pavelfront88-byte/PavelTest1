9675d4e6563de38170aadfdb20bf8ecfe865d512
Pavel <pavelfront88@>
2025-11-20 23:45:22 +0500

    add new branch save

--- src/main/java/org/example/Factorial.java
+++ src/main/java/org/example/Factorial.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class Factorial {
-}
+import java.math.BigInteger;
+public class Factorial
+{
+    public static BigInteger get(int n)
+    {
+        BigInteger result=n >=0? BigInteger.ONE : null;
+        for (int i=1; i<=n; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        return result;
+    }}
