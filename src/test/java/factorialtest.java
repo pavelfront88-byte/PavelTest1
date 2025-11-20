c0894fe7193098856d9796fe1ea3dc5adb5910bf
Pavel <pavelfront88@>
2025-11-21 00:05:58 +0500

    add new branch save

--- src/test/java/factorialtest.java
+++ src/test/java/factorialtest.java
@@ -13,25 +13,10 @@ public class factorialtest {
 
     @Test
     void naturalInt() {
-        Assertions.assertEquals(BigInteger.valueOf(1), factorialtest.get(1));
-        Assertions.assertEquals(BigInteger.valueOf(2), factorialtest.get(2));
-        Assertions.assertEquals(BigInteger.valueOf(399168), factorialtest.get(11));
-        Assertions.assertEquals(BigInteger.valueOf(4790016), factorialtest.get(12));
-    }
-
-    @Test
-    void naturalLong() {
-        Assertions.assertEquals(BigInteger.valueOf(62270208L), factorialtest.get(13));
-        Assertions.assertEquals(BigInteger.valueOf(871782912L), factorialtest.get(14));
-        Assertions.assertEquals(BigInteger.valueOf(1216451L), factorialtest.get(55));
-        Assertions.assertEquals(BigInteger.valueOf(2432902L), factorialtest.get(66));
-    }
-
-    @Test
-    void naturalBigInt() {
-        Assertions.assertEquals(new BigInteger("77170"), factorialtest.get(42));
-        Assertions.assertEquals(new BigInteger("999977"), factorialtest.get(44));
-        Assertions.assertEquals(new BigInteger("6654454"), factorialtest.get(111));
+        Assertions.assertEquals(BigInteger.valueOf(1), factorialtest.get(5));
+        Assertions.assertEquals(BigInteger.valueOf(2), factorialtest.get(6));
+        Assertions.assertEquals(BigInteger.valueOf(3), factorialtest.get(7));
+        Assertions.assertEquals(BigInteger.valueOf(4), factorialtest.get(8));
     }
 
     @Test
