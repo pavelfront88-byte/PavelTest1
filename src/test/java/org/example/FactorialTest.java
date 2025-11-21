e59f7a04ef8e5abaa63bd13302ee1e6287cc5fc5
Pavel <pavelfront88@>
2025-11-21 22:16:33 +0500

    add new branch save

--- src/test/java/org/example/FactorialTest.java
+++ src/test/java/org/example/FactorialTest.java
@@ -1,43 +1,29 @@
 package org.example;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.math.BigInteger;
 
-public class FactorialTest {
-
+public class FactorialTest
+{
     @Test
-    public void testFactorialZero() {
-        BigInteger result = Factorial.get(0);
-        Assert.assertEquals(result, BigInteger.ONE, "0# должен быть равен 1");
+    public void zero() {
+        Assert.assertEquals(get(0), BigInteger.valueOf(1));
     }
-
+    private static BigInteger get(int i)
+    {
+        return null;
+    }
     @Test
-    public void testFactorialOne() {
-        BigInteger result = Factorial.get(1);
-        Assert.assertEquals(result, BigInteger.ONE, "1# должен быть равен 1");
+    public void naturalInt()
+    {
+        Assert.assertEquals(get(5), BigInteger.valueOf(1));
+        Assert.assertEquals(get(6), BigInteger.valueOf(2));
+        Assert.assertEquals(get(7), BigInteger.valueOf(3));
+        Assert.assertEquals(get(8), BigInteger.valueOf(4));
     }
-
     @Test
-    public void testFactorialFive() {
-        BigInteger result = Factorial.get(5);
-        Assert.assertEquals(result, new BigInteger("120"), "5# должен быть равен 120");
-    }
-
-    @Test
-    public void testFactorialTen() {
-        BigInteger result = Factorial.get(10);
-        Assert.assertEquals(result, new BigInteger("3628800"), "10# должен быть равен 3 628 800");
-    }
-
-    @Test(expectedExceptions = NullPointerException.class)
-    public void testFactorialNegative() {
-        Factorial.get(-1);
-    }
-
-    @Test
-    public void testFactorialLargeNumber() {
-        BigInteger result = Factorial.get(20);
-        Assert.assertEquals(result, new BigInteger("2432902008176640000"), "20# должен быть равен 2 432 902 008 176 640 000");
-    }
-}
+    public void lessZero()
+    {
+        Assert.assertNull(get(-1));
+        Assert.assertNull(get(-2));
+    }}
