05b344e760adb61c6fdbd1231d6a26f3419da291
Pavel <pavelfront88@>
2025-11-21 20:55:46 +0500

    add new branch save

--- src/test/java/factorialtest.java
+++ src/test/java/factorialtest.java
@@ -2,26 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.math.BigInteger;
 
-public class factorialtest {
+public class factorialtest
+{
     @Test
-    void zero() {
+    void zero()
+    {
         Assertions.assertEquals(BigInteger.valueOf(1), factorialtest.get(0));
     }
-    private static BigInteger get(int i) {
+    private static BigInteger get(int i)
+    {
         return null;
     }
-
     @Test
-    void naturalInt() {
+    void naturalInt()
+    {
         Assertions.assertEquals(BigInteger.valueOf(1), factorialtest.get(5));
         Assertions.assertEquals(BigInteger.valueOf(2), factorialtest.get(6));
         Assertions.assertEquals(BigInteger.valueOf(3), factorialtest.get(7));
         Assertions.assertEquals(BigInteger.valueOf(4), factorialtest.get(8));
     }
-
     @Test
-    void lessZero() {
+    void lessZero()
+    {
         Assertions.assertNull(factorialtest.get(-1));
         Assertions.assertNull(factorialtest.get(-2));
-    }
-}
+    }}
